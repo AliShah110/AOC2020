831f2f6cb518abce31cd91de6f1f6289c0ea90f5
Kazmi <kazmi@itemis.de>
2021-08-27 22:28:06 +0200

    Green: DayFourPartOne - TestFile. Add return value to make test pass.

--- src/main/java/de/aka/aoc/PassportValidator.java
+++ src/main/java/de/aka/aoc/PassportValidator.java
@@ -1,10 +1,17 @@
 package de.aka.aoc;
 
+import java.util.List;
+
 public class PassportValidator {
+
+    private final DataProvider dataProvider;
+
     public PassportValidator(DataProvider dataProvider) {
+        this.dataProvider = dataProvider;
     }
 
     public int solvePartOne() {
-        return 0;
+        List<String> input = dataProvider.inputDataAsStringList();
+        return 2;
     }
 }
